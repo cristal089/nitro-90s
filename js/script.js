a0fb213251b0341866664153575b638b678bee89
Rafaela Cristina <betelgeusin@gmail.com>
2022-02-18 14:52:02 -0300

    Adicionando script para a funcionalidade de filtro do portfólio

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  
+  //Filtro do portfólio
+  $('.filter-btn').on('click', function(){
+    let type=$(this).attr('id');
+    let boxes=$('.project-box');
+    $('.main-btn').removeClass('active');
+    $(this).addClass('active');
+
+    if(type == 'trad-btn')
+      eachBoxes('trad', boxes);
+    else if(type == 'dig-btn')
+      eachBoxes('dig', boxes);
+    else if(type == 'street-btn')
+      eachBoxes('street', boxes);
+    else
+      eachBoxes('all', boxes);
+  });
+  function eachBoxes(type, boxes){
+    if(type=='all')
+      $(boxes).fadeIn();
+    else{
+      $(boxes).each(function(){
+        if(!$(this).hasClass(type))
+          $(this).fadeOut('slow');
+        else
+          $(this).fadeIn();
+      });
+    }
+  }
+});
