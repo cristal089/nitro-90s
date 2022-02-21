3384cbbb508d07782423b2ed8256c776b06b4499
Rafaela Cristina <betelgeusin@gmail.com>
2022-02-21 17:50:17 -0300

    Adicionando função de ampliar as imagens por meio de pop up

--- js/script.js
+++ js/script.js
@@ -50,4 +50,16 @@ $(document).ready(function(){
       scrollTop: $(scrollTo).offset().top - 70
     }, 350);
   }); 
+  //Pop up das imagens quando clicadas
+  var modalEle = document.querySelector(".modal");
+  var modalImage = document.querySelector(".modalImage");
+  Array.from(document.querySelectorAll(".img-fluid")).forEach(item => {
+    item.addEventListener("click", event => {
+        modalEle.style.display = "block";
+        modalImage.src = event.target.src;
+    });
+  });
+  document.querySelector(".close").addEventListener("click", () => {
+    modalEle.style.display = "none";
+  });
 });
